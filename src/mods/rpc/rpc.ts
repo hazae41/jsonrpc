c1d99e4c9b5a67af39a193f0cd96437c64e6c62d
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-10-17 14:03:06 +0700

    fix

--- src/mods/rpc/rpc.ts
+++ src/mods/rpc/rpc.ts
@@ -1,10 +1,12 @@
-import { RpcRequest, RpcRequestPreinit } from "./request.js"
+import { RpcParamfulRequestPreinit, RpcRequest, RpcRequestPreinit } from "./request.js"
 
 export class RpcCounter {
 
   id = 0
 
-  prepare<T>(init: RpcRequestPreinit<T>): RpcRequest<T> {
+  prepare<T>(init: RpcRequestPreinit<T>): RpcRequest<T>
+
+  prepare<T>(init: RpcParamfulRequestPreinit<T>): RpcRequest<T> {
     const { id = this.id++, method, params } = init
     return new RpcRequest(id, method, params)
   }
